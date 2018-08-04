8339fe516464a33095f31f30d8bd70d90cad4398
Matheus <matheus2324@htomail.com>
2018-08-04 15:53:51 -0300

    Parcial -> Implementation NewOrderService

--- GeekBurger.Orders.API/Services/Bus/NewOrderService.cs
+++ GeekBurger.Orders.API/Services/Bus/NewOrderService.cs
@@ -1,14 +1,19 @@
-﻿using GeekBurger.Orders.API.Contracts.Bus;
-using GeekBurger.Orders.API.Infra;
+﻿using GeekBurger.Orders.API.Contracts;
+using GeekBurger.Orders.API.Contracts.Bus;
 using GeekBurger.Orders.API.Services.Infra;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace GeekBurger.Orders.API.Services.Bus
 {
     public class NewOrderService : ServiceBusSub, INewOrderService
     {
+        public override string _topic { get; set; } = "NewOrder";
+        public override string _subscriptionName { get; set; } = "SubscriptionNewOrderPay";
+
+        public NewOrderService(IConfiguration configuration, ILogService logService)
+            : base(configuration, logService)
+        {
+        }
+        
     }
 }
--- GeekBurger.Orders.API/Services/Infra/ServiceBusSub.cs
+++ GeekBurger.Orders.API/Services/Infra/ServiceBusSub.cs
@@ -1,4 +1,8 @@
-﻿using GeekBurger.Orders.API.Contracts.Infra;
+﻿using GeekBurger.Orders.API.Contracts;
+using GeekBurger.Orders.API.Contracts.Infra;
+using GeekBurger.Orders.API.Infra;
+using Microsoft.Azure.Management.ServiceBus.Fluent;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +10,46 @@ using System.Threading.Tasks;
 
 namespace GeekBurger.Orders.API.Services.Infra
 {
-    public class ServiceBusSub : IServiceBusSub
+    public abstract class ServiceBusSub : IServiceBusSub
     {
+        public abstract string _topic { get; set; }
+        public abstract string _subscriptionName { get; set; }
+
+        private IConfiguration _configuration;
+        private Task _lastTask;
+        private IServiceBusNamespace _namespace;
+        private ILogService _logService;
+
+
+        public ServiceBusSub(IConfiguration configuration, ILogService logService)
+        {
+            _configuration = configuration;
+            _logService = logService;
+            _namespace = _configuration.GetServiceBusNamespace();
+            EnsureTopicIsCreated();
+            EnsureSubscriptionsIsCreated();
+        }
+
+        public void EnsureTopicIsCreated()
+        {
+            if (!_namespace.Topics
+                           .List()
+                           .Any(topic => topic.Name.Equals(_topic, StringComparison.InvariantCultureIgnoreCase)))
+                _namespace.Topics
+                          .Define(_topic)
+                          .WithSizeInMB(1024).Create();
+        }
+
+        public void EnsureSubscriptionsIsCreated()
+        {
+            var topic = _namespace.Topics.GetByName(_topic);
+
+            if (!topic.Subscriptions
+                      .List()
+                      .Any(subscription => subscription.Name.Equals(_subscriptionName, StringComparison.InvariantCultureIgnoreCase)))
+                topic.Subscriptions
+                     .Define(_subscriptionName)
+                     .Create();
+        }
     }
 }

2e8cbfa68b12cf0db26f27aec0c7d1580559e07a
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-04 14:15:10 -0300

    task -> order state na entidade order

--- GeekBurger.Orders.API/Model/Order.cs
+++ GeekBurger.Orders.API/Model/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeekBurger.Orders.Contract.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -11,5 +12,6 @@ namespace GeekBurger.Orders.API.Model
         public decimal Total { get; set; }
         public List<int> Products{get; set;}
         public List<int> ProductionIds { get; set; }
+        public OrderState State { get; set; }
     }
 }
